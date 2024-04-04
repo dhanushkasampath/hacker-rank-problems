530871116a4685a48670ce1ca597cad2641febb2
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-04-04 15:33:52 +0530

    identified the use of SUM

--- src/main/java/com/code/practise/leetcodeproblems/sql/basic_aggregate_functions/no19_queries_quality_and_percentage/answer.sql
+++ src/main/java/com/code/practise/leetcodeproblems/sql/basic_aggregate_functions/no19_queries_quality_and_percentage/answer.sql
@@ -1,6 +1,7 @@
 SELECT query_name, ROUND(AVG(RATING/POSITION), 2) AS quality,
 round(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) * 100 / count(*), 2) AS poor_query_percentage
 FROM Queries
+WHERE query_name IS NOT NULL
 GROUP BY query_name;
 
 --we can use "SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)" as a count one by one which match the given condition
---
+++ src/main/java/com/code/practise/leetcodeproblems/sql/basic_aggregate_functions/no20_monthly_transactions/answer.sql
@@ -0,0 +1,16 @@
+SELECT
+
+DATE_FORMAT(trans_date, '%Y-%m') AS month, country,
+COUNT(id) AS trans_count,
+SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END)  AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
+
+FROM Transactions
+GROUP BY month, country
+
+
+--GROUP BY should get the AS value
+--here it group by the combination of month and country
+--SUM(CASE WHEN state='approved' THEN 1 ELSE 0 END)    -> one by one count the records with approved state
+--SUM(CASE WHEN state='approved' THEN amount ELSE 0 END)   -> one by one get the sum of amount column whose state is approved
