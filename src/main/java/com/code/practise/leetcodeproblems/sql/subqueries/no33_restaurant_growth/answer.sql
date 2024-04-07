ee73740c6244bc2d6759ff269095796cc3654e78
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-04-07 11:20:20 +0530

    sql completed

--- src/main/java/com/code/practise/leetcodeproblems/sql/subqueries/no33_restaurant_growth/answer.sql
+++ src/main/java/com/code/practise/leetcodeproblems/sql/subqueries/no33_restaurant_growth/answer.sql
@@ -1,6 +1,21 @@
 SELECT
-    date,
-    revenue,
-    AVG(revenue) OVER (ORDER BY date ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS moving_avg
-FROM
-    sales;
+    visited_on,
+    (
+        SELECT SUM(amount)
+        FROM customer
+        WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+    ) AS amount,
+    ROUND(
+        (
+            SELECT SUM(amount) / 7
+            FROM customer
+            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY) AND c.visited_on
+        ),
+        2
+    ) AS average_amount
+FROM customer c
+WHERE visited_on >= (
+        SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY)
+        FROM customer
+    )
+GROUP BY visited_on;
