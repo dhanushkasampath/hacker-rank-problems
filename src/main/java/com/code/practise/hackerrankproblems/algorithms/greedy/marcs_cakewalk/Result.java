ddd8ce0cf69a54b7205dbc9b9d6bb9625284a087
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-04-28 12:06:34 +0530

    greedy approach

---
+++ src/main/java/com/code/practise/hackerrankproblems/algorithms/greedy/marcs_cakewalk/Result.java
@@ -0,0 +1,31 @@
+package com.code.practise.hackerrankproblems.algorithms.greedy.marcs_cakewalk;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+class Result {
+
+    /*
+     * Complete the 'marcsCakewalk' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY calorie as parameter.
+     */
+
+    public static long marcsCakewalk(List<Integer> calorie) {
+        // Write your code here
+        Collections.sort(calorie);
+        Collections.reverse(calorie);
+        long minimumMilesNecessary = 0;
+        for(int i=0; i< calorie.size(); i++){
+            minimumMilesNecessary += (long) (Math.pow(2, i) * calorie.get(i));
+        }
+        return minimumMilesNecessary;
+    }
+
+    public static void main(String[] args){
+        System.out.println(marcsCakewalk(Arrays.asList(5, 10, 7)));
+    }
+
+}
