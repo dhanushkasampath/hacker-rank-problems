a47853ba96bebb8d2739f98f82fdaa3dbb3ad25e
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-16 21:40:24 +0530

    java loops ll solved

---
+++ src/main/java/com/code/practise/hackerrankproblems/common/java_loops_ll/Solution.java
@@ -0,0 +1,45 @@
+package com.code.practise.hackerrankproblems.common.java_loops_ll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * This answer got accepted
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int noOfQueries = sc.nextInt();
+
+        List<List<Integer>> queryList = new ArrayList<>();
+
+        for(int i=0; i< noOfQueries; i++){
+            List<Integer> query = new ArrayList<>();
+            for(int j = 0; j < 3; j++){
+                query.add(sc.nextInt());
+            }
+            queryList.add(query);
+        }
+        printSeries(queryList);
+    }
+
+    private static void printSeries(List<List<Integer>> queryList){
+        for (List<Integer> query : queryList) {
+            int a = query.get(0);
+            int b = query.get(1);
+            int n = query.get(2);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            int last = 0;
+            for (int i=0; i<n;i++) {
+                int val = (int) (Math.pow(2, i) * b);
+                last = last + val;
+                int finalVal = a + last;
+                stringBuilder.append(finalVal);
+                stringBuilder.append(" ");
+            }
+            System.out.println(stringBuilder);
+        }
+    }
+}
