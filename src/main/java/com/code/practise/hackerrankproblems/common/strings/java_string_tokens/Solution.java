446c5c804dd0311760178db4a0296f5edb20345e
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-17 08:28:17 +0530

    java string tokens solved

---
+++ src/main/java/com/code/practise/hackerrankproblems/common/strings/java_string_tokens/Solution.java
@@ -0,0 +1,23 @@
+package com.code.practise.hackerrankproblems.common.strings.java_string_tokens;
+
+import java.util.Scanner;
+
+/**
+ * This answer got accepted
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.nextLine();
+        String[] arr = inputString.trim().split("[!,?._'@ ]+");
+
+        if(arr.length == 1 && arr[0].isEmpty()){
+            System.out.println(0);
+        } else {
+            System.out.println(arr.length);
+        }
+        for(String s : arr){
+            System.out.println(s);
+        }
+    }
+}
