1943a69fcf89cf3752ef3172ab3835b17850d841
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-23 22:14:30 +0530

    sed command

---
+++ src/main/java/com/code/practise/hackerrankproblems/linux_shell/grep_sed_awk/medium/sed_2/solution.sh
@@ -0,0 +1,7 @@
+sed 's/thy/your/gi'
+
+# s: The substitute command.
+# thy: The search pattern.
+# your: The replacement string.
+# g: Global flag to replace all occurrences in each line.
+# i: Case-insensitive flag to match "thy" in any combination of uppercase and lowercase letters.
